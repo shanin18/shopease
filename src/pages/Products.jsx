9c16c8e5652f82c92f614928a056d7d5f07431e3
shanin18 <shaninh12@gmail.com>
2024-05-26 17:14:10 +0600

    added necessary pages

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h1>This is Product page</h1>
+        </div>
+    );
+};
+
+export default Products;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            this is SignUp page
+        </div>
+    );
+};
+
+export default SignUp;
