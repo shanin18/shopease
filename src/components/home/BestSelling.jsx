beccd878ece642ea32bcfafea48bb5a711f70a62
shanin18 <shaninh12@gmail.com>
2024-06-10 13:55:00 +0600

    removed buttons form best selling and flash sales components

--- src/components/home/BestSelling.jsx
+++ src/components/home/BestSelling.jsx
@@ -7,9 +7,6 @@ const BestSelling = ({ data }) => {
     <section>
       <div className="flex flex-wrap items-end justify-between mb-8 gap-4 p-5 md:p-0">
         <SectionHeader category="This Month" title="Best Selling Products" />
-        <button className="bg-red-500 hover:bg-red-600 text-white px-6 sm:px-8 py-2 sm:py-[10px] rounded text-sm sm:text-base">
-          View All
-        </button>
       </div>
       <Products data={data} />
     </section>
--- src/components/home/FlashSales.jsx
+++ src/components/home/FlashSales.jsx
@@ -11,11 +11,6 @@ const FlashSales = ({ data }) => {
         <Countdown targetDate="2024-06-11T23:59:59" color="black" />
       </div>
       <Products data={data} />
-      <div className="text-center mb-12 mt-8">
-        <button className="bg-red-500 hover:bg-red-600 text-white px-8 py-[10px] rounded">
-          View All Products
-        </button>
-      </div>
     </section>
   );
 };
